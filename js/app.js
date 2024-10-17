a53d89ddda9157459c84dc34347314f03a51d559
Adrian Gavela Rodriguez <adriangavela09@gmail.com>
2024-10-17 14:19:35 +0200

    casi terminado

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@ function obtenerFechaActual(){
   let dia = fecha.getDate();
   let mes = fecha.getMonth() + 1;
   let ano = fecha.getFullYear();
-
-  if(dia < 10){
+  console.log(mes);
+  if(dia <= 10){
     dia = dia + '0';
   }else{
     dia;
@@ -91,7 +91,6 @@ function formatearNumero(numero) {
     numeroNuevo = numeroNuevo + ',' + parteDecimal; 
   }
   
-
   if(isNaN(numero)){
     return null;
   }
@@ -102,13 +101,70 @@ function formatearNumero(numero) {
 
 // Función que recibe un string en formato dd/mm/yyyy y devuelve el número de días que faltan para el 6 de Enero del año siguiente
 function reyesMagos(fecha) {
- 
+  let fechaCadena = fecha.split('/');
+
+  let dia = parseInt(fechaCadena[0]);
+  let mes = parseInt(fechaCadena[1])-1;
+  let año = parseInt(fechaCadena[2]);
+
+  let fechaUsuario = new Date(año, mes, dia);
+  let fechaReyes = new Date(año, 0, 6);
+  
+  if(fechaUsuario.getDay() === 6 && fechaUsuario.getMonth() === 0){
+    return 0;
+  }
+
+  if(fechaUsuario > fechaReyes){
+    fechaReyes = new Date(año + 1, 0 ,6);
+  }
+
+  let milisegundos = fechaReyes.getTime() - fechaUsuario.getTime();
+  let dias = Math.ceil(milisegundos / (1000 * 60 * 60 * 24));
+  
+  
+  if(isNaN(fechaUsuario)){
+    return null;
+  }
+
+  return dias;
+
 }
 
 
 // Ejercicio 10: Convertir a lowerCamelCase
 function camelCase(cadena) {
+
+  if(typeof(cadena) != 'string'){
+    return null;
+  }
+
+  let  acentos = ['á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Í', 'Ó', 'Ú'];
+  let  sinAcento = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
   
+  for(let k = 0; k < acentos.length; k++){
+    if(cadena.includes(acentos[k])){
+      cadena = cadena.replace(acentos[k], sinAcento[k])
+    }
+  }
+ 
+
+  let cadenaArray = cadena.split(' ');
+  let nuevaCadena = '';
+
+  for(let k = 0; k < cadenaArray.length; k++){
+    let letras = cadenaArray[k].charAt(0).toUpperCase();
+    nuevaCadena += cadenaArray[k].replace(cadenaArray[k].charAt(0), letras);
+    
+  }
+  let primeraLetra = nuevaCadena.charAt(0).toLocaleLowerCase();
+  nuevaCadena = nuevaCadena.replace(nuevaCadena[0], primeraLetra)
+
+  if(cadenaArray == ''){
+    return '';
+  }
+
+
+  return nuevaCadena;
 }
 
 
@@ -121,13 +177,47 @@ function diasParaNavidad(){
 // Función que genera un número aleatorio entre comienzo y fin
 function calcularAleatorio(comienzo, fin){
 
+  if(isNaN(comienzo)){
+    return null;
+  }
+
+  if(comienzo < 0){
+    return null;
+  }
+
+  if(comienzo > fin){
+    return null;
+  }
+
+  return Math.floor(Math.random() * (comienzo - fin +1) + fin);
 }
 
 
 
 //   función para verificar si una cadena es un palíndromo sin utilizar expresiones regulares ni programación funcional
 function esPalindromo(cadena){
+  if(!isNaN(cadena)){
+    return false;
+  }
 
+  let cadenaReverse = '';
+  let cadenaArray = cadena.split(' ').join('').toLowerCase();
+  console.log(cadenaArray);
+  for(let k = 0; k < cadenaArray.length; k++){
+    cadenaReverse = cadenaArray.split('').reverse().join('').toLowerCase();
+    console.log(cadenaReverse);
+    if(cadenaReverse === cadenaArray){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  if(cadenaArray === ''){
+    return false;
+  }
+
+ 
   
 }
 
